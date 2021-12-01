dcf4ea28c09a4c30930c8396278d6322aefc3ec3
Aemitrius <67407608+Aemitrius@users.noreply.github.com>
2021-12-01 19:10:37 +0530

    Create hash.h

---
+++ hash.h
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdbool.h>
+
+#define STD_FILE_SYSTEM_SIZE     20480
+
+typedef struct HASHTABLE_NODE {
+
+    mode_t mode; 
+    void * entry;
+    char * path;
+
+    bool is_linked;
+    struct HASHTABLE_NODE * next;
+    
+} HASHTABLE_NODE;
